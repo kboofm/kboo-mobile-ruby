7d99c32cd04b0c4ee2485610673db14b05a296ef
epicodus <student@epicodus.com>
2013-10-14 09:43:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kboo::Application.config.secret_key_base = 'cbe33940a91e055d44b7560eb7d54d0702836594b5290a854aa210198a77d8d9c86e10aadf68c10c11343ebbc0b3d52a48a7e539098e6cb3bc2aea5d07e7ebea'
