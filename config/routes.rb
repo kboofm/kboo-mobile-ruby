0814c20f2004c63613692e06c3bb0d064690819b
epicodus <student@epicodus.com>
2013-10-14 17:47:43 -0700

    forth cherry pick fix for users branch finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Kboo::Application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+  
+  resources :users, :only => [:edit, :update, :index]
+  devise_for :users
+  root 'users#index'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+>>>>>>> 258528a... navbar
 
   devise_for :users
   resources :users, :only => [:edit, :update, :index, :show]
