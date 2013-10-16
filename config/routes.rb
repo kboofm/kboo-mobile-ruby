625172a74227369379ce27570c82e4e9124850d8
epicodus <student@epicodus.com>
2013-10-16 09:59:28 -0700

    Resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Kboo::Application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   resources :users, :only => [:edit, :update, :index, :show]
   resources :programs
-=======
+
   resources :episode_audio
   resources :episodes
->>>>>>> master
+
 
   root 'static_pages#home'
 end
