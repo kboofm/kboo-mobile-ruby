d59208398ce03033c13ccb03b007da3e144a85cf
epicodus <student@epicodus.com>
2013-10-18 16:29:05 -0700

    episodes done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,7 @@
 class Episode < ActiveRecord::Base
   belongs_to :program
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-
+  default_scope -> { order('date DESC') }
   validates :title, :short_description,
                     :long_description,
                     # :audio_promo,
