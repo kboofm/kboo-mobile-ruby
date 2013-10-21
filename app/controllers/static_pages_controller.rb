fb6f80fafccb0746a6420efecbea9ff155e4a713
Robert Grayson & Jason Rodriguez <student@epicodus.com>
2013-10-21 09:48:29 -0700

    more static page controller work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,57 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
+
+  def test
+  end
+
+  def our_story
+  end
+
+  def faq
+  end
+
+  def staff 
+  end
+
+  def board
+  end
+
+  def committies 
+  end
+
+  def governance
+  end
+
+  def strategic_plan
+  end
+
+  def known_issues
+  end
+
+  def contact
+  end
+
+  def press_area
+  end
+
+  def host_benefit
+  end
+
+  def trainings
+  end
+
+  def youth
+  end
+
+  def donate_your_car
+  end
+
+  def underwriter
+  end
+
+  def advertise
+  end
+
 end
