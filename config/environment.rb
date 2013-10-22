bb8709ad06ac1d947173d540e912ba3e46bf1e99
Gabriel S Valdivia & Michael Kaiser-Nyman <student@epicodus.com>
2013-10-22 09:42:18 -0700

    clean up files

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
 # Initialize the Rails application.
 Kboo::Application.initialize!
