6e7b75080d9d1146b1407fd5db9e91f264803f12
Hunter Meyer <huntermeyer@live.com>
2013-10-25 15:06:25 -0700

    Rename spec/integration folder to spec/features

---
+++ spec/features/episodes_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'Episodes pages' do
+  subject { page }
+
+  describe 'list episodes' do
+    it 'should display a recent episode' do
+      FactoryGirl.create(:episode)  
+      visit root_path
+      page.should have_content 'jenka'
+    end
+  end
+end
---
+++ spec/features/programs_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'Program pages' do
+  subject { page }
+      
+  describe 'new page' do
+    it 'should create a program' do
+      category = FactoryGirl.create(:category) 
+      admin = FactoryGirl.create(:admin) 
+      program = FactoryGirl.build(:program)
+      visit new_program_path
+      fill_in 'Title',          with: program.title
+      fill_in 'Date',           with: program.date
+      fill_in 'Time',           with: program.time
+      fill_in 'Description',    with: program.description
+      select  category.name,    from: 'program_category_id'
+      select  admin.email,      from: 'program_hosts_attributes_0_user_id'
+      click_button 'Save Program'
+      page.should have_content 'Build a Rails app'
+    end
+  end
+
+  def login
+    FactoryGirl.create(:admin)
+    visit new_user_session_path
+    fill_in 'Email',    with: 'admin@kboo.org'
+    fill_in 'Password', with: '12345678'
+    click_button 'Sign in'
+  end
+
+  describe 'edit page' do
+    it 'should update a program' do
+      login
+      FactoryGirl.create(:category) 
+      program = FactoryGirl.create(:program)
+      visit edit_program_path(program.id)
+      fill_in 'Title', with: 'Updating a Program title'
+      select  'Technology',     from: 'program_category_id'
+      click_button 'Submit' 
+      page.should have_content 'Updating a Program title'
+    end
+  end
+end
