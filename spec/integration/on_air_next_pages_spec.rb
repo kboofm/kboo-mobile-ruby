06aa493b3c0d7f74fe1b8cae90cbe95192bbf440
Hunter Meyer <huntermeyer@live.com>
2013-10-25 15:06:46 -0700

    Refactor and clean up syntax

--- spec/integration/on_air_next_pages_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'home page' do
-  it 'should show the program title that will be ON AIR NEXT', :vcr do
-    OnAirNext.instance.fetch
-    visit root_path
-    expect(page).to have_content 'The Thom Hartmann Program'
-  end
-end
