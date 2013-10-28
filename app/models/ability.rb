fa4f27049cf0aa878321b8519fa6713412fb5d56
Moneer <mbcomm@gmail.com>
2013-10-28 11:44:32 -0700

    fix css for mvp_mobile(main site css still broken) and added role column to user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,11 +12,6 @@ class Ability
       else
         can :read, :all
       end  
-
-
-
-
-
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022175345) do
+ActiveRecord::Schema.define(version: 20131028183706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20131022175345) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
