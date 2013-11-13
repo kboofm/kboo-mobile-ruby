882e30cbaa9656d6ec0e612cf6f7c7b194a4edc8
Hunter Meyer <huntermeyer@live.com>
2013-11-13 15:02:43 -0800

    Clean syntax

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Episode do
-  it { should belong_to :program }
+  it { should belong_to            :program }
   it { should validate_presence_of :program }
   it { should validate_presence_of :title }
   it { should validate_presence_of :short_description }
