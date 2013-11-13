6bdaeeb04ff766bd7cfbc3499a3cfe01f293cd6e
Hunter Meyer <huntermeyer@live.com>
2013-11-13 12:47:47 -0800

    Add spec for creating an episode with valid info

--- spec/features/episodes_pages_spec.rb
+++ spec/features/episodes_pages_spec.rb
@@ -2,19 +2,32 @@ require 'spec_helper'
 
 describe 'Episodes pages' do
 
-  describe 'list episodes' do
+  describe 'index' do
     it 'should display a recent episode' do
       episode = FactoryGirl.create(:episode)
       visit root_path
       page.should have_content episode.title
     end
-
-    it 'lets you create a program with valid info'
-
-    it 'gives you an error if you try to create a program with invalid info'
-
-    it 'lets you edit a program'
-
-    it 'lets you delete a program'
   end
+
+  describe 'new' do
+    it 'lets you create a program with valid info' do
+      episode = FactoryGirl.build(:episode)
+      admin = FactoryGirl.create(:admin)
+      login_as(admin)
+      visit new_episode_path
+      select  episode.program.title,  from: 'episode_program_id'
+      fill_in 'Title',                with: episode.title
+      fill_in 'Short description',    with: episode.short_description
+      fill_in 'Long description',     with: episode.long_description
+      click_button 'Submit'
+      page.should have_content episode.title
+    end
+  end
+    # it 'gives you an error if you try to create a program with invalid info'
+
+    # it 'lets you edit a program'
+
+    # it 'lets you delete a program'
+
 end
