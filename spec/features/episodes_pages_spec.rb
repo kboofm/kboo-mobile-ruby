217f0046bb956642e558831ad27e7eb06308af3c
Hunter Meyer <huntermeyer@live.com>
2013-11-13 13:01:18 -0800

    Refactor tests

--- spec/features/episodes_pages_spec.rb
+++ spec/features/episodes_pages_spec.rb
@@ -13,8 +13,7 @@ describe 'Episodes pages' do
   describe 'new' do
     it 'lets you create a program with valid info' do
       episode = FactoryGirl.build(:episode)
-      admin = FactoryGirl.create(:admin)
-      login_as(admin)
+      login_as(FactoryGirl.create(:admin))
       visit new_episode_path
       select  episode.program.title,  from: 'episode_program_id'
       fill_in 'Title',                with: episode.title
@@ -26,16 +25,14 @@ describe 'Episodes pages' do
 
     it 'gives an error if you try to create an episode invalid info' do
       episode = FactoryGirl.build(:episode)
-      admin = FactoryGirl.create(:admin)
-      login_as(admin)
+      login_as(FactoryGirl.create(:admin))
       visit new_episode_path
       click_button 'Submit'
       page.should have_content 'can\'t be blank'
     end
   end
-    # it 'gives you an error if you try to create a program with invalid info'
+  
 
-    # it 'lets you edit a program'
 
     # it 'lets you delete a program'
 
