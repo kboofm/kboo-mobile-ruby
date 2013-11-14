9e950d9e3ddceb5f68f9020deb48970874a38d68
Hunter Meyer <huntermeyer@live.com>
2013-11-13 18:33:49 -0800

    Change double- to single-quotes

--- app/assets/javascripts/jplayer_jquery.js
+++ app/assets/javascripts/jplayer_jquery.js
@@ -1,24 +1,21 @@
 function updatePlayer(url){
-  $("#jquery_jplayer_1").jPlayer({
+  $('#jquery_jplayer_1').jPlayer({
     ready: function () { 
-      $(this).jPlayer("setMedia", { 
+      $(this).jPlayer('setMedia', { 
         mp3: url
-
       }); 
-      $(this).jPlayer("play");
+      $(this).jPlayer('play');
     },
-    swfPath: "js",
-    supplied: "mp3, m4a, oga",
-
+    swfPath: 'js',
+    supplied: 'mp3, m4a, oga',
   }); 
-  $("#jquery_jplayer_1").jPlayer("setMedia", { 
+  $('#jquery_jplayer_1').jPlayer('setMedia', { 
     mp3: url
   }); 
-  $("#jquery_jplayer_1").jPlayer("play");
+  $('#jquery_jplayer_1').jPlayer('play');
 }
-
 
 function stop() {
 //jplayer code to stop the mp3 music by assign stop string into jPlayer method which div tag of jquery_jplayer_1
-  $("#jquery_jplayer_1").jPlayer("destroy");
+  $('#jquery_jplayer_1').jPlayer('destroy');
 }
