6cbab412d345694da1a68d0b97cab1b8af78b29f
Hunter Meyer <huntermeyer@live.com>
2013-11-13 18:32:51 -0800

    Remove commented code, adjust indention

--- app/assets/javascripts/home_nav.js
+++ app/assets/javascripts/home_nav.js
@@ -1,32 +1,20 @@
 $(document).ready(function (){
-    $(window).scroll(function() {
-      var scrollTop = 120;
-      if($(window).scrollTop() && $(window).scrollTop() >= scrollTop ){
-            $('.lowernav').css({
-                position : 'fixed',
-                top : '0'
-            });
-        }
-        if($(window).scrollTop() < scrollTop){
-                    $('.lowernav').removeAttr('style');  
-                }
-    });  
-    // $(window).scroll(function() {
-    //   var scrollTop = 78;
-    //   if($(window).scrollTop() && $(window).scrollTop() >= scrollTop ){
-    //         $('.lowernav').css({
-    //             position : 'fixed',
-    //             top : '0'
-    //         });
-    //     }
-    //     if($(window).scrollTop() < scrollTop){
-    //                 $('.lowernav').removeAttr('style');  
-    //             }
-    // });
-    //FIXME
-      $('a.html5').click(function() {
-        var url = $(this).attr('href');  
-        $("#jquery_jplayer_1").jPlayer("setMedia", {mp3: url}).jPlayer("play");
-        return false;  
-      }); 
+  $(window).scroll(function() {
+    var scrollTop = 120;
+    if($(window).scrollTop() && $(window).scrollTop() >= scrollTop ){
+      $('.lowernav').css({
+        position : 'fixed',
+        top : '0'
+      });
+    }
+    if($(window).scrollTop() < scrollTop){
+      $('.lowernav').removeAttr('style');  
+    }
+  });  
+
+  $('a.html5').click(function() {
+    var url = $(this).attr('href');  
+    $("#jquery_jplayer_1").jPlayer("setMedia", {mp3: url}).jPlayer("play");
+    return false;  
+  }); 
 });
