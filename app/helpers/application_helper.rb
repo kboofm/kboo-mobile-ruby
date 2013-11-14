6fb2a0fa254f4b4fe063d718691bb54166009146
Hunter Meyer <huntermeyer@live.com>
2013-11-13 17:14:09 -0800

    Test full_title helper for dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'KBOO'
+    if page_title.empty?
+      base_title + ' Community Radio'
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
