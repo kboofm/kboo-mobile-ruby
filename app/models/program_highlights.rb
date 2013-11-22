e4b257fa8a747d8e19cb46d628a7778e9bd252ec
Hunter Meyer <huntermeyer@live.com>
2013-11-21 17:01:06 -0800

    Rename schedule to program_schedule

--- app/models/program_highlights.rb
+++ app/models/program_highlights.rb
@@ -5,7 +5,6 @@ class ProgramHighlights < ActiveRecord::Base
   serialize :highlights, Array
   self.table_name = 'program_highlights'
   include ActiveRecord::Singleton
-  attr_accessor :titles, :urls
 
   def fetch
     url = "http://www.kboo.fm"
